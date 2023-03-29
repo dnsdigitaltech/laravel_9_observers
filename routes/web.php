f6498e0933f65485376a872878619c980170ad6e
Davi Bernardo <davi.bernardo@sbahq.org>
2023-03-29 20:29:37 -0300

    Criando Model, Controller, view de product

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\ProductController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,5 +25,15 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::resource('products', ProductController::class);
+   // Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
+   Route::controller(ProductController::class)->group(function(){
+        Route::get('products', 'index')->name('products.index');
+        Route::post('products', 'store')->name('products.store');
+        Route::get('products/create', 'create')->name('products.create');
+        Route::get('products/{product}', 'show')->name('products.show');
+        Route::put('products/{product}', 'update')->name('products.update');
+        Route::delete('products/{product}', 'destroy')->name('products.destroy');
+        Route::get('products/{product}/edit', 'edit')->name('products.edit');
+    });
 });
+Route::post('/upload', [UploadController::class, 'store'])->name('upload');
