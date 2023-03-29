53153b9a219f5e9f9f2da0d94fd176f084bb549d
Davi Bernardo <davi.bernardo@sbahq.org>
2023-03-29 19:56:27 -0300

    Criando as migrations products e product_histories

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('products', ProductController::class);
+});
